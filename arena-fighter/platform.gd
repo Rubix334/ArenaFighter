94c8e20b7a0548f36edfb8167bc9c463435a5897
Rubix334 <2260334@flourbluffschools.org>
2026-02-18 12:35:38 -0600

    separate one-way collision for each player
    Complete

--- arena-fighter/platform.gd
+++ arena-fighter/platform.gd
@@ -1,14 +1,25 @@
 extends CSGBox3D
 @onready var player1: Player = $"../Player1"
+@onready var player2: Player = $"../Player2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	use_collision = false
-
+	#use_collision = false
+	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (player1.position.y-6) > position.y:
-		use_collision = true
+		set_collision_layer_value(1, true)
+		set_collision_mask_value(1, true)
 	else:
-		use_collision = false
+		set_collision_mask_value(1, false)
+		set_collision_layer_value(1, false)
+	
+	if (player2.position.y-6) > position.y:
+		set_collision_layer_value(2, true)
+		set_collision_mask_value(2, true)
+	else:
+		set_collision_layer_value(2, false)
+		set_collision_mask_value(2, false)
